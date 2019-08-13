70ea66552b47d641646568e6339c0cbb261adc6b
zeljkoczl <zeljkoczl@gmail.com>
2019-08-13 23:18:49 +0200

    final

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -9,7 +9,7 @@ function changeBG(){
         "url(https://images.unsplash.com/photo-1444228250525-3d441b642d12?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
         "url(https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
         "url(https://images.unsplash.com/photo-1445308394109-4ec2920981b1?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)"
-      ]
+      ];
     
     if(counter === imgs.length) counter = 0;
     $("body").css("background-image", imgs[counter]);
