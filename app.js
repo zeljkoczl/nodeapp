6e5b36bc07f19b65f4e3b3f238c61cb8a52baada
zeljkoczl <zeljkoczl@gmail.com>
2019-08-13 19:59:33 +0200

    final

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+mongoose.connect("mongodb://zeljkoc:colovic93@ds263127.mlab.com:63127/campmaster");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
